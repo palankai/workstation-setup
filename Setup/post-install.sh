a581e0c78ebb6e85e2f82b69757936d30ecd04db
Csaba Palankai <csaba.palankai@gmail.com>
2022-06-29 21:47:48 +0100

    Little refactoring

--- Setup/post-install.sh
+++ Setup/post-install.sh
@@ -32,21 +32,19 @@ ensure_repository https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 
 ln -sf $PWD/src/sensitive/dotfiles/ssh_config $HOME/.ssh/config
 
-ln -sf $PWD/src/dotfiles/gitignore_global $HOME/.gitignore_global
-ln -sf $PWD/src/sensitive/dotfiles/gitconfig $HOME/.gitconfig
+ln -sf $PWD/dotfiles/gitignore_global $HOME/.gitignore_global
+ln -sf $PWD/sensitive/dotfiles/gitconfig $HOME/.gitconfig
 
-ln -sf $PWD/src/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/src/dotfiles/vimrc.vim $HOME/.vimrc
+ln -sf $PWD/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $PWD/dotfiles/vimrc.vim $HOME/.vimrc
 
-ln -sf $PWD/src/dotfiles/zprofile $HOME/.zprofile
-ln -sf $PWD/src/dotfiles/zshrc $HOME/.zshrc
+ln -sf $PWD/dotfiles/zprofile $HOME/.zprofile
+ln -sf $PWD/dotfiles/zshrc $HOME/.zshrc
 
-ln -sf $PWD/src/dotfiles/condarc.yaml $HOME/.condarc
+ln -sf $PWD/dotfiles/condarc.yaml $HOME/.condarc
 
-ln -sf $PWD/src/sensitive/dotfiles/aws.conf $HOME/.aws/config
-ln -sf $PWD/src/sensitive/dotfiles/docker-config.json $HOME/.docker/config.json
-
-ln -sf $PWD/src/bin/aws-vault $HOME/opt/bin
+ln -sf $PWD/sensitive/dotfiles/aws.conf $HOME/.aws/config
+ln -sf $PWD/sensitive/dotfiles/docker-config.json $HOME/.docker/config.json
 
 # https://apple.stackexchange.com/questions/69223/how-to-replace-mac-os-x-utilities-with-gnu-core-utilities
 # https://dev.to/dnsmichi/use-homebrew-bundle-to-manage-software-installation-on-macos-1223
