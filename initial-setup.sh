19bc5374d9a36df4b270bd2ab211e4c56bc1d074
Csaba Palankai <csaba.palankai@gmail.com>
2022-06-29 22:23:58 +0100

    Recompile initial-setup

--- initial-setup.sh
+++ initial-setup.sh
@@ -13,7 +13,6 @@ GPG_AGENT_FILE="$HOME/.gnupg/gpg-agent.conf"
 
 LOG_INFO_OUTPUT="/dev/stderr"
 LOG_DEBUG_OUTPUT="/dev/stderr"
-LOG_RESULT_OUTPUT="workstation-setup-log.txt"
 
 INSTALLATION_BASE_PATH=$HOME/opt
 
@@ -21,7 +20,156 @@ REPOSITORY_NAME=workstation-setup
 REPOSITORY=git@github.com:palankai/$REPOSITORY_NAME.git
 REPOSITORY_PATH=$INSTALLATION_BASE_PATH/$REPOSITORY_NAME
 
+WORKSTATION_INSTALLATION_PATH=$INSTALLATION_BASE_PATH/$REPOSITORY_NAME
+LOG_RESULT_OUTPUT="$WORKSTATION_INSTALLATION_PATH/workstation-setup-log.txt"
+
+
 set -e
+
+function initial_setup {
+    log_info "Initialisation"
+    ensure_local_file
+    ensure_essentials
+    ensure_gpg
+    clone_repository
+    gpg_post_setup
+
+    next_step_instructions
+}
+
+function ensure_local_file {
+    log_info "Ensure local profile file"
+    # SELECT workstation profile
+    if ! test_line_exists $PROFILE_FILE "WORKSTATION="; then
+        local WORKSTATION=$(menu "Select installation profile" personal work quit)
+        if [[ $WORKSTATION == "quit" ]]; then
+            echo "bye"
+            exit 1
+        fi
+        echo "export WORKSTATION=$WORKSTATION" >> $PROFILE_FILE
+        echo "Selected profile: $WORKSTATION"
+    fi
+    log_info "Ensure ARCH in local profile"
+    # STORE Arch
+    if ! test_line_exists $PROFILE_FILE "ARCH="; then
+        echo "ARCH=$(uname -m)" >> $PROFILE_FILE
+    fi
+    log_info "Ensure BREW_HOME in local profile"
+    # STORE Brew Home
+    if ! test_line_exists $PROFILE_FILE "BREW_HOME="; then
+        if [[ $(uname -m) -eq "x86_64" ]]; then
+            echo "BREW_HOME=/usr/local" >> $PROFILE_FILE
+        else
+            echo "BREW_HOME=/opt/homebrew" >> $PROFILE_FILE
+        fi
+    fi
+
+    # STORE System Python Path
+    ensure_line $PROFILE_FILE "SYSTEM_PYTHON=" "SYSTEM_PYTHON=/usr/bin/python3"
+    ensure_line $PROFILE_FILE "SYSTEM_PIP=" "SYSTEM_PIP=/usr/bin/pip3"
+    ensure_line $PROFILE_FILE "SYSTEM_PYTHON_INSTALL_PATH=" "SYSTEM_PYTHON_INSTALL_PATH=$(/usr/bin/python3 -m site --user-base)/bin"
+    ensure_line $PROFILE_FILE "WORKSTATION_INSTALLATION_PATH=" "WORKSTATION_INSTALLATION_PATH=$WORKSTATION_INSTALLATION_PATH"
+
+    # LOAD what we have
+    source $PROFILE_FILE
+
+    log_result "Env: WORKSTATION=$WORKSTATION"
+    log_result "Env: WORKSTATION_INSTALLATION_PATH=$WORKSTATION_INSTALLATION_PATH"
+    log_result "Env: ARCH=$ARCH"
+    log_result "Env: BREW_HOME=$BREW_HOME"
+    log_result "Env: SYSTEM_PYTHON=$SYSTEM_PYTHON"
+    log_result "Env: SYSTEM_PIP=$SYSTEM_PIP"
+    log_result "Env: SYSTEM_PYTHON_INSTALL_PATH=$SYSTEM_PYTHON_INSTALL_PATH"
+    log_result "Env: PYTHONPATH=$PYTHONPATH"
+}
+
+function ensure_essentials {
+    log_info "Installing essentials..."
+    if ! is_installed brew; then
+        log_info "Installing: brew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        log_result "Brew installed"
+    else
+        log_info "Updating: brew..."
+        brew update
+        brew upgrade
+        log_result "Brew updated"
+    fi
+    eval $($(brew --prefix)/bin/brew shellenv)
+    log_info "Homebrew paths setup in this environment"
+
+    brew_install mas
+    log_info "Installing xcode-select"
+    xcode-select --install || true
+    log_result "xcode-selected: installed"
+}
+
+function ensure_gpg {
+    if ! is_installed gpg; then
+        brew install gnupg
+    fi
+    if ! is_installed pinentry-mac; then
+        brew install pinentry-mac
+    fi
+    gpg -k
+    local pinentry_path=$(which pinentry-mac)
+    local target_file=.local_profile
+    mkdir -p ~/.gnupg/
+
+    ensure_line $GPG_AGENT_FILE pinentry-program "pinentry-program $pinentry_path"
+
+    ensure_line $PROFILE_FILE "PINENTRY_PATH=" "PINENTRY_PATH=$pinentry_path"
+    ensure_line $PROFILE_FILE "GPGCONF_EXEC=" "GPGCONF_EXEC=$(which gpgconf)"
+
+    source $PROFILE_FILE
+    log_result "Env: PINENTRY_PATH=$pinentry_path"
+    log_result "Env: GPGCONF_EXEC=$GPGCONF_EXEC"
+
+    restart_gpg
+    log_result "Initial GPG Setup complete"
+}
+
+function clone_repository {
+    mkdir -p $INSTALLATION_BASE_PATH
+    ensure_repository $REPOSITORY $WORKSTATION_INSTALLATION_PATH
+}
+
+function gpg_post_setup {
+    ln -sf $WORKSTATION_INSTALLATION_PATH/dotfiles/gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+    ln -sf $WORKSTATION_INSTALLATION_PATH/dotfiles/gnupg/gnupg/gpg.conf $HOME/.gnupg/gpg.conf
+    ln -sf $WORKSTATION_INSTALLATION_PATH/dotfiles/gnupg/scdaemon.conf $HOME/.gnupg/scdaemon.conf
+    log_result "GPG Config files: linked"
+
+    restart_gpg
+    mkdir -p $HOME/Library/LaunchAgents/
+
+    local escaped=$(echo $GPGCONF_EXEC | sed 's/\//\\\//g')
+    sed "s/{{ GPGCONF_EXEC }}/$escaped/g" $WORKSTATION_INSTALLATION_PATH/templates/homebrew.gpg.gpg-agent.plist >$HOME/Library/LaunchAgents/homebrew.gpg.gpg-agent.plist
+    launchctl load -F $HOME/Library/LaunchAgents/homebrew.gpg.gpg-agent.plist 2>/dev/null
+    log_result "Homebrew GPG LaunchAgent: installed & started"
+
+    cp $WORKSTATION_INSTALLATION_PATH/templates/link-ssh-auth-sock.plist $HOME/Library/LaunchAgents/link-ssh-auth-sock.plist
+
+    launchctl load -F $HOME/Library/LaunchAgents/link-ssh-auth-sock.plist 2>/dev/null
+    log_result "SSH Auth Sock LaunchAgent: installed & started"
+}
+
+function next_step_instructions {
+    log_br
+    log_result "Initial setup completed"
+    log_info "Next Steps:"
+    log_br
+    log_info "cd $WORKSTATION_INSTALLATION_PATH"
+    log_info "make"
+}
+
+function restart_gpg {
+    log_info "Restarting GPG..."
+    killall gpg-agent || true
+    gpg-agent --daemon --homedir $HOME/.gnupg
+    export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
+    log_result "GPG restarted"
+}
 
 function ensure_repository {
     local url=$1
@@ -39,6 +187,7 @@ function ensure_repository {
 
     log_info "$url Updating submodules..."
     (cd $repo_path; git submodule update --init --recursive)
+    (cd $repo_path; git submodule foreach git checkout master)
     log_result "$url submodules: Updated"
 }
 
@@ -176,151 +325,5 @@ function log_result {
     echo "$1" >> $LOG_INFO_OUTPUT
 }
 
-# High level functions
-
-function ensure_local_file {
-    log_info "Ensure local profile file"
-    # SELECT workstation profile
-    if ! test_line_exists $PROFILE_FILE "WORKSTATION="; then
-        local WORKSTATION=$(menu "Select installation profile" personal work quit)
-        if [[ $WORKSTATION == "quit" ]]; then
-            echo "bye"
-            exit 1
-        fi
-        echo "WORKSTATION=$WORKSTATION" >> $PROFILE_FILE
-        echo "Selected profile: $WORKSTATION"
-    fi
-    log_info "Ensure ARCH in local profile"
-    # STORE Arch
-    if ! test_line_exists $PROFILE_FILE "ARCH="; then
-        echo "ARCH=$(uname -m)" >> $PROFILE_FILE
-    fi
-    log_info "Ensure BREW_HOME in local profile"
-    # STORE Brew Home
-    if ! test_line_exists $PROFILE_FILE "BREW_HOME="; then
-        if [[ $(uname -m) -eq "x86_64" ]]; then
-            echo "BREW_HOME=/usr/local" >> $PROFILE_FILE
-        else
-            echo "BREW_HOME=/opt/homebrew" >> $PROFILE_FILE
-        fi
-    fi
-
-    # STORE System Python Path
-    ensure_line $PROFILE_FILE "SYSTEM_PYTHON=" "SYSTEM_PYTHON=/usr/bin/python3"
-    ensure_line $PROFILE_FILE "SYSTEM_PIP=" "SYSTEM_PIP=/usr/bin/pip3"
-    ensure_line $PROFILE_FILE "SYSTEM_PYTHON_INSTALL_PATH=" "SYSTEM_PYTHON_INSTALL_PATH=$(/usr/bin/python3 -m site --user-base)/bin"
-
-    # LOAD what we have
-    source $PROFILE_FILE
-
-    log_result "Env: WORKSTATION=$WORKSTATION"
-    log_result "Env: ARCH=$ARCH"
-    log_result "Env: BREW_HOME=$BREW_HOME"
-    log_result "Env: SYSTEM_PYTHON=$SYSTEM_PYTHON"
-    log_result "Env: SYSTEM_PIP=$SYSTEM_PIP"
-    log_result "Env: SYSTEM_PYTHON_INSTALL_PATH=$SYSTEM_PYTHON_INSTALL_PATH"
-    log_result "Env: PYTHONPATH=$PYTHONPATH"
-}
-
-function ensure_essentials {
-    log_info "Installing essentials..."
-    if ! is_installed brew; then
-        log_info "Installing: brew..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        log_result "Brew installed"
-    else
-        log_info "Updating: brew..."
-        brew update
-        brew upgrade
-        log_result "Brew updated"
-    fi
-    eval $($(brew --prefix)/bin/brew shellenv)
-    log_info "Homebrew paths setup in this environment"
-
-    pip_install Jinja2
-    brew_install mas
-    log_info "Installing xcode-select"
-    xcode-select --install || true
-    log_result "xcode-selected: installed"
-}
-
-function ensure_gpg {
-    if ! is_installed gpg; then
-        brew install gnupg
-    fi
-    if ! is_installed pinentry-mac; then
-        brew install pinentry-mac
-    fi
-    gpg -k
-    local pinentry_path=$(which pinentry-mac)
-    local target_file=.local_profile
-    mkdir -p ~/.gnupg/
-
-    ensure_line $GPG_AGENT_FILE pinentry-program "pinentry-program $pinentry_path"
-
-    ensure_line $PROFILE_FILE "PINENTRY_PATH=" "PINENTRY_PATH=$pinentry_path"
-    ensure_line $PROFILE_FILE "GPGCONF_EXEC=" "GPGCONF_EXEC=$(which gpgconf)"
-
-    source $PROFILE_FILE
-    log_result "Env: PINENTRY_PATH=$pinentry_path"
-    log_result "Env: GPGCONF_EXEC=$GPGCONF_EXEC"
-
-    restart_gpg
-    log_result "Initial GPG Setup complete"
-}
-
-function clone_repository {
-    mkdir -p $INSTALLATION_BASE_PATH
-    ensure_repository $REPOSITORY $REPOSITORY_PATH
-}
-
-function gpg_post_setup {
-    ln -sf $REPOSITORY_PATH/src/dotfiles/gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
-    ln -sf $REPOSITORY_PATH/src/dotfiles/gnupg/gnupg/gpg.conf $HOME/.gnupg/gpg.conf
-    ln -sf $REPOSITORY_PATH/src/dotfiles/gnupg/scdaemon.conf $HOME/.gnupg/scdaemon.conf
-    log_result "GPG Config files: linked"
-
-    restart_gpg
-    mkdir -p $HOME/Library/LaunchAgents/
-
-    local escaped=$(echo $GPGCONF_EXEC | sed 's/\//\\\//g')
-    sed "s/{{ GPGCONF_EXEC }}/$escaped/g" src/templates/homebrew.gpg.gpg-agent.plist >$HOME/Library/LaunchAgents/homebrew.gpg.gpg-agent.plist
-    launchctl load -F $HOME/Library/LaunchAgents/homebrew.gpg.gpg-agent.plist 2>/dev/null
-    log_result "Homebrew GPG LaunchAgent: installed & started"
-
-    cp src/templates/link-ssh-auth-sock.plist $HOME/Library/LaunchAgents/link-ssh-auth-sock.plist
-
-    launchctl load -F $HOME/Library/LaunchAgents/link-ssh-auth-sock.plist 2>/dev/null
-    log_result "SSH Auth Sock LaunchAgent: installed & started"
-}
-
-function next_step_instructions {
-    log_br
-    log_result "Initial setup completed"
-    log_info "Next Steps:"
-    log_br
-    log_info "cd $REPOSITORY_PATH"
-    log_info "make"
-}
-
-function restart_gpg {
-    log_info "Restarting GPG..."
-    killall gpg-agent || true
-    gpg-agent --daemon --homedir $HOME/.gnupg
-    export SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh
-    log_result "GPG restarted"
-}
-
-
-function initial_setup {
-    log_info "Initialisation"
-    ensure_local_file
-    ensure_essentials
-    ensure_gpg
-    clone_repository
-    gpg_post_setup
-
-    next_step_instructions
-}
 
 initial_setup
