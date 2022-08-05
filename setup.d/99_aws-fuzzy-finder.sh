ed398ed91c53e4f59b83d90c252892100c767e63
Csaba Palankai <csaba.palankai@gmail.com>
2022-08-05 13:57:09 +0100

    Install Imagemagick and fix aws-fuzzy-finder

--- setup.d/99_aws-fuzzy-finder.sh
+++ setup.d/99_aws-fuzzy-finder.sh
@@ -1,7 +1,7 @@
-if [ "$ARCH" = "arm64" ]; then
-    ensure_repository https://github.com/epoplavskis/aws-fuzzy-finder.git /tmp/aws-fuzzy-finder
-    (cd /tmp/aws-fuzzy-finder/; $SYSTEM_PIP install .)
-    rm -rf /tmp/aws-fuzzy-finder
-else
-    $SYSTEM_PIP install aws-fuzzy-finder
-fi
+# if [ "$ARCH" = "arm64" ]; then
+#     ensure_repository https://github.com/epoplavskis/aws-fuzzy-finder.git /tmp/aws-fuzzy-finder
+#     (cd /tmp/aws-fuzzy-finder/; $SYSTEM_PIP install .)
+#     rm -rf /tmp/aws-fuzzy-finder
+# else
+#     $SYSTEM_PIP install aws-fuzzy-finder
+# fi
