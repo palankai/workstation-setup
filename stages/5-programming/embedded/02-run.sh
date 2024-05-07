2147405977150a02fce3832a4901f0ff129c0946
Csaba Palankai <csaba.palankai@gmail.com>
2024-05-07 09:09:47 +0100

    Embedded stuf

--- stages/5-programming/embedded/02-run.sh
+++ stages/5-programming/embedded/02-run.sh
@@ -2,6 +2,3 @@ rustup toolchain install nightly
 cargo install cargo-generate
 cargo install ravedude
 cargo install espup
-cargo install espflash
-cargo install cargo-espflash
-cargo install ldproxy
