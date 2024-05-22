d314c7cc997c587258d7b75a4bdba7630b209097
Csaba Palankai <csaba.palankai@gmail.com>
2024-05-22 16:27:06 +0100

    Powerlevel10k installation 1st part

--- stages/5-programming/rust/02-run.sh
+++ stages/5-programming/rust/02-run.sh
@@ -1,7 +1,2 @@
 rustup toolchain install nightly
 cargo install cargo-generate
-cargo install ravedude
-cargo install espup
-cargo install espflash
-cargo install cargo-espflash
-cargo install ldproxy
